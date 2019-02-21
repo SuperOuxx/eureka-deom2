f8b0b2ed798d3269c785d3663440ccd8253db7cf
SuperOuxx <342494436@qq.com>
2019-02-21 15:34:04 +0800

    SpringCloud Eureka demo

---
+++ src/test/java/com/example/eurekadeom2/EurekaDeom2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.eurekadeom2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaDeom2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
